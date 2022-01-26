c073e75ee222c272ca1db05d08c402da21194d73
ChristinaTracey <98213564+ChristinaTracey@users.noreply.github.com>
2022-01-26 16:13:35 +0300

    Add files via upload

---
+++ salary.py
@@ -0,0 +1,9 @@
+#input
+name=input("Enter your name: ")
+print(name)
+age=int(input("Enter age: "))
+print(age)
+hours=int(input("Enter hours: "))
+rate=float(input("Enter rate: "))
+pay=hours*rate
+print("Gross Pay is: " ,pay)
---
+++ variables.py
@@ -0,0 +1,16 @@
+#variables
+#power
+power=6**20
+print("power operator", power)
+x=23+5/5*3-2**3+(34-10)
+#rem
+print(21%6)
+rem=21%6
+print("remainder is :", rem)
+print(x)
+name="Halima"
+print(type(name))
+age=20
+print(type(age))
+price=100.23
+print(type(price))
