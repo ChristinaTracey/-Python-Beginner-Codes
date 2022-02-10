bd5c1547be04b27bc8af1115786dd403daa960f4
ChristinaTracey <98213564+ChristinaTracey@users.noreply.github.com>
2022-02-10 14:13:06 +0300

    Add files via upload

--- salary.py
+++ salary.py
@@ -1,9 +1,10 @@
-#input
-name=input("Enter your name: ")
-print(name)
-age=int(input("Enter age: "))
-print(age)
-hours=int(input("Enter hours: "))
-rate=float(input("Enter rate: "))
-pay=hours*rate
-print("Gross Pay is: " ,pay)
+salary=float(input("enter salary:"))
+yos=float(input("eneter year of service:"))
+if yos>10:
+  print("bonus is: ",0.01*salary)
+elif yos>=6 and yos<=10:
+  print("bonus is: ",0.08*salary)
+else:
+  print("bonus is: ",0.05*salary)
+  
+  
