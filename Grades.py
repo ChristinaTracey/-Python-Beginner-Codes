aaa90129333ce9752735346707de0afbc2639079
Christina <98213564+ChristinaTracey@users.noreply.github.com>
2022-02-13 20:20:47 +0300

    Add files via upload

---
+++ Grades.py
@@ -0,0 +1,13 @@
+scores=int(input("Enter your score:"))
+if scores >= 90 and scores <= 100:
+        print("A")
+elif scores >= 80 and scores <= 89:
+        print("B")
+elif scores >= 70 and scores<= 79:
+        print("C")
+elif scores >= 60 and scores<= 69:
+        print("D")
+elif scores >= 50 and scores <= 59:
+        print("E")
+else:
+        print("F")
